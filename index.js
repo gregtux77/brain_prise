0b3607dcf3f11d1962571dac9f617992be664bd6
gregtux77 <34283203+gregtux77@users.noreply.github.com>
2017-12-17 19:46:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var install = require('./lib/install.js');
 var uninstall = require('./lib/uninstall.js');
+var send = require('./lib/send.js');
 //var exec = require('./lib/exec.js');
 
 
@@ -9,6 +10,7 @@ module.exports = function(sails) {
    
     
     return {
+	    send:send,
      install: install,
 		uninstall: uninstall
     };
